3e4360dad3626b37bf90d1abcfa0f7e2fde40ceb
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-12-31 13:05:24 +0100

    Next level properties are explicitly tested

---
+++ DrdPlus/Tests/PersonProperties/NextLevelsPropertiesTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace DrdPlus\Tests\PersonProperties;
+
+use DrdPlus\Person\ProfessionLevels\ProfessionLevels;
+use DrdPlus\PersonProperties\NextLevelsProperties;
+use DrdPlus\Properties\Base\Agility;
+use DrdPlus\Properties\Base\Charisma;
+use DrdPlus\Properties\Base\Intelligence;
+use DrdPlus\Properties\Base\Knack;
+use DrdPlus\Properties\Base\Strength;
+use DrdPlus\Properties\Base\Will;
+use DrdPlus\Tests\Tools\TestWithMockery;
+
+class NextLevelsPropertiesTest extends TestWithMockery
+{
+    /**
+     * @test
+     */
+    public function I_can_get_properties()
+    {
+        $sut = new NextLevelsProperties($this->createProfessionLevels(
+            $strength = 1, $agility = 2, $knack = 3, $will = 4, $intelligence = 5, $charisma = 6
+        ));
+        $this->assertSame(Strength::getIt($strength), $sut->getNextLevelsStrength());
+        $this->assertSame(Agility::getIt($agility), $sut->getNextLevelsAgility());
+        $this->assertSame(Knack::getIt($knack), $sut->getNextLevelsKnack());
+        $this->assertSame(Will::getIt($will), $sut->getNextLevelsWill());
+        $this->assertSame(Intelligence::getIt($intelligence), $sut->getNextLevelsIntelligence());
+        $this->assertSame(Charisma::getIt($charisma), $sut->getNextLevelsCharisma());
+    }
+
+    /**
+     * @param int $strength
+     * @param int $agility
+     * @param int $knack
+     * @param int $will
+     * @param int $intelligence
+     * @param int $charisma
+     * @return \Mockery\MockInterface|ProfessionLevels
+     */
+    private function createProfessionLevels($strength, $agility, $knack, $will, $intelligence, $charisma)
+    {
+        $professionLevels = $this->mockery(ProfessionLevels::class);
+        $professionLevels->shouldReceive('getNextLevelsStrengthModifier')
+            ->andReturn($strength);
+        $professionLevels->shouldReceive('getNextLevelsAgilityModifier')
+            ->andReturn($agility);
+        $professionLevels->shouldReceive('getNextLevelsKnackModifier')
+            ->andReturn($knack);
+        $professionLevels->shouldReceive('getNextLevelsWillModifier')
+            ->andReturn($will);
+        $professionLevels->shouldReceive('getNextLevelsIntelligenceModifier')
+            ->andReturn($intelligence);
+        $professionLevels->shouldReceive('getNextLevelsCharismaModifier')
+            ->andReturn($charisma);
+
+        return $professionLevels;
+    }
+}
