9ba9ee573991186c52894692d20c282c0f811159
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-04-12 14:13:52 +0200

    Exceptions hierarchy tested

---
+++ DrdPlus/Tests/PersonProperties/ExceptionsHierarchyTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace DrdPlus\Tests\PersonProperties;
+
+use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+
+class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+{
+    protected function getTestedNamespace()
+    {
+        return $this->getRootNamespace();
+    }
+
+    protected function getRootNamespace()
+    {
+        return str_replace('\Tests', '', __NAMESPACE__);
+    }
+
+}
