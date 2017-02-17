6b94aebdcce19b346cdf18196aa333ee60fe8e4a
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-17 07:53:18 +0100

    Fixed tests after a library incompatible change

--- DrdPlus/Tests/PropertiesByLevels/PropertiesByLevelsExceptionsHierarchyTest.php
+++ DrdPlus/Tests/PropertiesByLevels/PropertiesByLevelsExceptionsHierarchyTest.php
@@ -1,15 +1,21 @@
 <?php
 namespace DrdPlus\Tests\PropertiesByLevels;
 
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
-class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+class PropertiesByLevelsExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
